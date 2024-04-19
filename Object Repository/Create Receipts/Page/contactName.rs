81d02e60143e66d06d68d20c60bc115a627d8251
hmanikandan <haariadhii@gmail.com>
2024-04-19 12:42:04 -0300

    Added TC's

---
+++ Object Repository/Create Receipts/Page/contactName.rs
@@ -0,0 +1,130 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>contactName</name>
+   <tag></tag>
+   <elementGuidId>f9618874-4748-4e75-9169-c760b0b781b4</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>(.//*[normalize-space(text()) and normalize-space(.)='Receive From'])[1]/following::a[6]</value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value>a.dropdown-item.ui-menu-item-wrapper.text-truncate.ui-state-active</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorCollection>
+      <entry>
+         <key>SMART_LOCATOR</key>
+         <value>internal:role=link[name=&quot;Hari Manikandan&quot;i]</value>
+      </entry>
+   </smartLocatorCollection>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>a</value>
+      <webElementGuid>94dc4a43-3d1c-4e24-be3c-62bc7b694645</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>href</name>
+      <type>Main</type>
+      <value>#</value>
+      <webElementGuid>d9cdb04e-4c62-4d57-944f-2d5280dee189</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>dropdown-item ui-menu-item-wrapper text-truncate ui-state-active</value>
+      <webElementGuid>35273bd4-122d-4158-be84-31dd1a0d3ca3</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>Hari Manikandan</value>
+      <webElementGuid>ac61731a-c34f-488c-abc3-1767413ae7ba</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>/html[1]/body[@class=&quot;o_web_client&quot;]/div[@class=&quot;o_action_manager&quot;]/div[@class=&quot;o_form_view o_view_controller o_action&quot;]/div[@class=&quot;o_form_view_container&quot;]/div[@class=&quot;o_content&quot;]/div[@class=&quot;o_form_renderer o_form_editable d-flex flex-column&quot;]/div[@class=&quot;o_form_sheet_bg&quot;]/div[@class=&quot;o_form_sheet position-relative&quot;]/div[@class=&quot;o_group row align-items-start&quot;]/div[@class=&quot;o_inner_group grid col-lg-6&quot;]/div[@class=&quot;o_wrap_field d-flex d-sm-contents flex-column mb-3 mb-sm-0&quot;]/div[@class=&quot;o_cell flex-grow-1 flex-sm-grow-0&quot;]/div[@class=&quot;o_field_widget o_field_many2one&quot;]/div[@class=&quot;o_field_many2one_selection&quot;]/div[@class=&quot;o_input_dropdown&quot;]/div[@class=&quot;o-autocomplete dropdown&quot;]/ul[@class=&quot;o-autocomplete--dropdown-menu ui-widget show dropdown-menu ui-autocomplete&quot;]/li[@class=&quot;o-autocomplete--dropdown-item ui-menu-item d-block&quot;]/a[@class=&quot;dropdown-item ui-menu-item-wrapper text-truncate ui-state-active&quot;]</value>
+      <webElementGuid>133cac99-f959-4197-82f4-058c1233c32f</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Receive From'])[1]/following::a[6]</value>
+      <webElementGuid>6e0e9ee8-4ed5-4348-875d-76b2104bd7e7</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Draft'])[2]/following::a[6]</value>
+      <webElementGuid>fec970cc-8c60-49ef-be53-47d263eb8416</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Operation Type'])[1]/preceding::a[5]</value>
+      <webElementGuid>65670c7c-425c-4e7c-81b2-3ee380b40884</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Destination Location'])[1]/preceding::a[5]</value>
+      <webElementGuid>d2d797bc-720a-45ef-90de-1aa88884325a</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>//*/text()[normalize-space(.)='Hari Manikandan']/parent::*</value>
+      <webElementGuid>27485582-e404-4f24-8084-babc375010bd</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:href</name>
+      <type>Main</type>
+      <value>(//a[contains(@href, '#')])[6]</value>
+      <webElementGuid>1be3392f-6502-4133-8d23-2b36b1042cc5</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//li[6]/a</value>
+      <webElementGuid>8681c7d5-cc02-4f84-975a-ad01f9278e38</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//a[@href = '#' and (text() = 'Hari Manikandan' or . = 'Hari Manikandan')]</value>
+      <webElementGuid>bf44708b-71ba-420e-83a5-b2b33520f836</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
---
+++ Object Repository/Create Receipts/Page/productName.rs
@@ -0,0 +1,130 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>productName</name>
+   <tag></tag>
+   <elementGuidId>441aac79-3227-455f-9649-bb7d081b7cfd</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>(.//*[normalize-space(text()) and normalize-space(.)='Demand'])[1]/following::a[5]</value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value>a.dropdown-item.ui-menu-item-wrapper.text-truncate.ui-state-active</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorCollection>
+      <entry>
+         <key>SMART_LOCATOR</key>
+         <value>internal:role=link[name=&quot;8K Monitor&quot;i]</value>
+      </entry>
+   </smartLocatorCollection>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>a</value>
+      <webElementGuid>e10ce8de-e632-43fa-8a00-daf1b1474b33</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>href</name>
+      <type>Main</type>
+      <value>#</value>
+      <webElementGuid>25405226-0ffb-417e-b325-fbfb22363355</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>dropdown-item ui-menu-item-wrapper text-truncate ui-state-active</value>
+      <webElementGuid>b1d42ca2-c137-4f7d-9eef-2d9538292ed0</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>8K Monitor</value>
+      <webElementGuid>94660d22-7312-4b22-ae87-ad550d2867ec</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>/html[1]/body[@class=&quot;o_web_client&quot;]/div[@class=&quot;o_action_manager&quot;]/div[@class=&quot;o_form_view o_view_controller o_action&quot;]/div[@class=&quot;o_form_view_container&quot;]/div[@class=&quot;o_content&quot;]/div[@class=&quot;o_form_renderer o_form_editable d-flex flex-column o_form_dirty&quot;]/div[@class=&quot;o_form_sheet_bg&quot;]/div[@class=&quot;o_form_sheet position-relative&quot;]/div[@class=&quot;o_notebook d-flex w-100 horizontal flex-column&quot;]/div[@class=&quot;o_notebook_content tab-content&quot;]/div[@class=&quot;tab-pane active fade show&quot;]/div[@class=&quot;o_field_widget o_field_stock_move_one2many o_field_one2many&quot;]/div[@class=&quot;o_list_view o_field_x2many o_field_x2many_list&quot;]/div[@class=&quot;o_list_renderer o_renderer table-responsive&quot;]/table[@class=&quot;o_list_table table table-sm table-hover position-relative mb-0 o_list_table_ungrouped table-striped&quot;]/tbody[@class=&quot;ui-sortable&quot;]/tr[@class=&quot;o_data_row o_selected_row&quot;]/td[@class=&quot;o_data_cell cursor-pointer o_field_cell o_list_many2one o_required_modifier&quot;]/div[@class=&quot;o_field_widget o_required_modifier o_field_many2one&quot;]/div[@class=&quot;o_field_many2one_selection&quot;]/div[@class=&quot;o_input_dropdown&quot;]/div[@class=&quot;o-autocomplete dropdown&quot;]/ul[@class=&quot;o-autocomplete--dropdown-menu ui-widget show dropdown-menu ui-autocomplete&quot;]/li[@class=&quot;o-autocomplete--dropdown-item ui-menu-item d-block&quot;]/a[@class=&quot;dropdown-item ui-menu-item-wrapper text-truncate ui-state-active&quot;]</value>
+      <webElementGuid>04c48d11-c473-4b63-9ddb-423bc09f3c87</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Demand'])[1]/following::a[5]</value>
+      <webElementGuid>36dcefa1-a9b0-4a9c-8be4-b909ac4b3913</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Product'])[1]/following::a[5]</value>
+      <webElementGuid>4728e853-e884-40d9-bb35-54ea89e5d2f9</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='​'])[1]/preceding::a[7]</value>
+      <webElementGuid>800376b1-48f2-4bae-8ca5-98dc03c5b787</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='​'])[2]/preceding::a[7]</value>
+      <webElementGuid>a21fa5e5-7a5b-41d2-8d28-0095dc6e3ee4</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>//*/text()[normalize-space(.)='8K Monitor']/parent::*</value>
+      <webElementGuid>11f2b70e-a38b-482b-be49-54c9252605b2</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:href</name>
+      <type>Main</type>
+      <value>(//a[contains(@href, '#')])[8]</value>
+      <webElementGuid>f7c59bcb-a8ca-4492-8053-3843ee40b732</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//li[5]/a</value>
+      <webElementGuid>0fa9395d-1277-4a30-b580-7622b0e802c7</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//a[@href = '#' and (text() = '8K Monitor' or . = '8K Monitor')]</value>
+      <webElementGuid>b14ebe0a-67e9-4459-8b2c-4956e6ec2725</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
